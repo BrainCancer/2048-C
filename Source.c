aca7ceebe41af055f94ba3c98d566916aa99fa13
BrainCancer <kobad2@gmail.com>
2014-04-28 23:08:12 -0700

    Updated algorithm of moving cells

--- Source.c
+++ Source.c
@@ -1,6 +1,9 @@
+/* Kobylianskii Dmitrii */
 #include "stdlib.h"
 #include "time.h"
-void Rand(int a[][4], int H, int W)
+#include "source.h"
+
+void Rand(Cell a[][4], int H, int W)
 {
 	int k, i, j;
 	for (k = 0; k < 2; k++)
@@ -9,70 +12,78 @@ void Rand(int a[][4], int H, int W)
 		srand(time(NULL));
 		i = rand() % (H - 1);
 		j = rand() % (W - 1);
-		if (a[i][j] == 0)
+		if (a[i][j].value == 0)
 		{
-			a[i][j] = 2;
+			a[i][j].value = 2;
 		}
 	}
 }
 
-void Move(int a[][4], int x, int y, int d)
+void Move(Cell a[][4], int x, int y, int d)
 {
 	switch (d)
 	{
 	case 1:
 	{
-					if (a[y + 1][x] == a[y][x])
+					if (a[y + 1][x].value == a[y][x].value && a[y][x].move != 1 && (y + 1) <= 0)
 					{
-						a[y][x] = 0;
-						a[y + 1][x] *= 2;
+						a[y][x].value = 0;
+						a[y + 1][x].value *= 2;
+						a[y + 1][x].move = 1;
 					}
-					else if (a[y + 1][x] == 0)
+					else if (a[y + 1][x].value == 0 && a[y][x].move != 1 && (y + 1) <= 3)
 					{
-						a[y + 1][x] = a[y][x];
-						a[y][x] = 0;
+						a[y + 1][x].value = a[y][x].value;
+						a[y][x].value = 0;
+						a[y + 1][x].move = 1;
 					}
 					break;
 	}
 	case 2:
 	{
-					if (a[y - 1][x] == a[y][x])
+					if (a[y - 1][x].value == a[y][x].value && a[y][x].move != 1 && (y - 1) >= 0)
 					{
-						a[y][x] = 0;
-						a[y - 1][x] *= 2;
+						a[y][x].value = 0;
+						a[y - 1][x].value *= 2;
+						a[y - 1][x].move = 1;
 					}
-					else if (a[y - 1][x] == 0 && (y - 1) >= 0)
+					else if (a[y - 1][x].value == 0 && (y - 1) >= 0 && a[y][x].move != 1)
 					{
-						a[y - 1][x] = a[y][x];
-						a[y][x] = 0;
+						a[y - 1][x].value = a[y][x].value;
+						a[y][x].value = 0;
+						a[y - 1][x].move = 1;
 					}
 					break;
 	}
 	case 3:
 	{
-					if (a[y][x + 1] == a[y][x])
+					if (a[y][x + 1].value == a[y][x].value && a[y][x].move != 1 && (x + 1) <= 3)
 					{
-						a[y][x] = 0;
-						a[y][x + 1] *= 2;
+						a[y][x].value = 0;
+						a[y][x + 1].value *= 2;
+						a[y][x + 1].move = 1;
 					}
-					else if (a[y][x + 1] == 0)
+					else if (a[y][x + 1].value == 0 && a[y][x].move != 1 && (x + 1) <= 3)
 					{
-						a[y][x + 1] = a[y][x];
-						a[y][x] = 0;
+						a[y][x + 1].value = a[y][x].value;
+						a[y][x].value = 0;
+						a[y][x + 1].move = 1;
 					}
 					break;
 	}
 	case 4:
 	{
-					if (a[y][x - 1] == a[y][x])
+					if (a[y][x - 1].value == a[y][x].value && a[y][x].move != 1 && (x - 1) >= 0)
 					{
-						a[y][x] = 0;
-						a[y][x - 1] *= 2;
+						a[y][x].value = 0;
+						a[y][x - 1].value *= 2;
+						a[y][x - 1].move = 1;
 					}
-					else if (a[y][x - 1] == 0)
+					else if (a[y][x - 1].value == 0 && a[y][x].move != 1 && (x - 1) >= 0)
 					{
-						a[y][x - 1] = a[y][x];
-						a[y][x] = 0;
+						a[y][x - 1].value = a[y][x].value;
+						a[y][x].value = 0;
+						a[y][x - 1].move = 1;
 					}
 					break;
 	}
